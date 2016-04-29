28b35bca3cf3a4355cd55ce443b9332b503a9079
Soheil Norouzi <soheil.nrz@gmail.com>
2016-04-28 17:35:05 -0700

    Move the frame up when keyboard is editting bottomText

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -11,7 +11,8 @@ import Darwin
 
 class ViewController: UIViewController,
     UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate {
+    UINavigationControllerDelegate,
+    UITextFieldDelegate {
 
     
     @IBOutlet weak var albumButton: UIButton!
@@ -36,19 +37,18 @@ class ViewController: UIViewController,
         bottomText.text = "BOTTOM"
         topText.textAlignment = NSTextAlignment.Center
         bottomText.textAlignment = NSTextAlignment.Center
-
-
-        
+        topText.delegate = self
+        bottomText.delegate = self
         let memeTextAttributes = [
             NSStrokeColorAttributeName : UIColor.blackColor(),
             NSForegroundColorAttributeName : UIColor.whiteColor(),
             NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
             NSStrokeWidthAttributeName : -4.0
         ]
-        
         topText.defaultTextAttributes = memeTextAttributes
         bottomText.defaultTextAttributes = memeTextAttributes
 
+        subscribeToKeyboardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
@@ -56,6 +56,11 @@ class ViewController: UIViewController,
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotification()
+    }
+    
     @IBAction func cancelButtonPressed(sender: AnyObject) {
         exit(0)
     }
@@ -82,7 +87,8 @@ class ViewController: UIViewController,
     
     
     
-    // MARK: Delegate Method Implementations
+    // MARK: ImagePickerControllerDelegate Implementations
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         print("image selected")
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -97,7 +103,53 @@ class ViewController: UIViewController,
     }
     
     
+    // MARK: UITextFieldDelegate Implementations
     
+    func textFieldDidBeginEditing(textField: UITextField) {
+        print("in textFieldDidBeginEditing")
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("in textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Keyboard Notification Subscription
+    
+    func subscribeToKeyboardNotifications() {
+        print("in subscribeToKeyboardNotifications")
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print("in keyboardWillShow")
+        if bottomText.isFirstResponder() {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        print("in keyboardWillHide")
+        if bottomText.isFirstResponder() {
+            self.view.frame.origin.y += getKeyboardHeight(notification)
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func unsubscribeFromKeyboardNotification() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
 
 }
 
