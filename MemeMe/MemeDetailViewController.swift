ab76d9e8db417a08a19eb23d21215750942fb2b4
Soheil Norouzi <soheil.nrz@gmail.com>
2016-05-03 08:11:53 -0700

    Add MemeDetailVC to storyboard

---
+++ MemeMe/MemeDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by soheiln on 5/3/16.
+//  Copyright © 2016 soheiln. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
